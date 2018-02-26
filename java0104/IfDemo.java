173cac578791363a869f3d0f90346cda34210646
Lxin2017 <34757553+Lxin2017@users.noreply.github.com>
2018-02-26 17:22:36 +0800

    java0104

---
+++ java0104/IfDemo.java
@@ -0,0 +1,25 @@
+class IfDemo 
+{
+	public static void main(String[] args) 
+	{
+		
+		int x = 3;
+		if(x==3||x==4||x==5)
+		{
+		  System.out.println(x+"´º¼¾");
+	    }
+		 else if(x==6||x==7||x==8)
+		{
+		  System.out.println(x+"ÏÄ¼¾");
+	    }
+		 else if(x==9||x==10||x==11)
+		{
+		  System.out.println(x+"Çï¼¾");
+	    }
+		 else if(x==12||x==1||x==2)
+		{
+		  System.out.println(x+"¶¬¼¾");
+	    }
+		System.out.println("Hello World!");
+	}
+}
